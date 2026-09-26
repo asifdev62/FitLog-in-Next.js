9acfebac001ba8534c57c00cbc0a50c1d32422ce
890asif <192508892+890asif@users.noreply.github.com>
2026-09-26 22:52:32 +0600

    api chaing

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,6 +1,8 @@
 import { Workout } from "@/types/Workout";
 
-const API_URL = "https://api.abcz.workers.dev/api/fitlog";
+// const API_URL = "https://api.abcz.workers.dev/api/fitlog";
+
+ const API_URL = "https://api.api-store.workers.dev/api/fitlog";
 
 export const getWorkouts = async (): Promise<Workout[]> => {
   const response = await fetch(API_URL);
