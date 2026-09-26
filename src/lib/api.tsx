14a45e22beaedf08dd474bc130094cc0e24e1fbf
890asif <192508892+890asif@users.noreply.github.com>
2026-09-26 23:11:29 +0600

    api change

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,6 +1,6 @@
 import { Workout } from "@/types/Workout";
 
- const API_URL = "https://api.api-store.workers.dev/api/fitlog";
+ const API_URL = "https://api.api-store.workers.dev/api/fitlog"
 
 export const getWorkouts = async (): Promise<Workout[]> => {
   const response = await fetch(API_URL);
